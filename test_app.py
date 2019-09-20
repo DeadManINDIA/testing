b44820bedd9d584bd620fb81e7e64d8543c0b904
DeadManIndia <deadmanindia001@gmail.com>
2019-09-20 12:04:41 +0530

    testting

--- test_app.py
+++ test_app.py
@@ -18,7 +18,7 @@ from app import test
 class TestHelloApp(unittest.TestCase):
 
   def test_test(self):
-    self.assertEqual(test(), "testing!\n")
+    self.assertEqual(test(), "synopsys!\n")
 
 if __name__ == '__main__':
   unittest.main()
