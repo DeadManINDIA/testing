f420d2e42883aaf4de77b9205c190f122bf01257
DeadManIndia <deadmanindia001@gmail.com>
2019-10-01 21:31:02 +0530

    Synopsys

--- app.py
+++ app.py
@@ -14,11 +14,11 @@
 
 # [START hello-app]
 from flask import Flask
-app = Flask('testing')
+app = Flask('synopsys')
 
 @app.route('/')
 def test():
-  return "testing!\n"
+  return "synopsys!\n"
 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = 8080)
