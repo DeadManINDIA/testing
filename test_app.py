0bfb4252ce1e170ef2ac82f9ad5d138a3e6c2b50
DeadManIndia <deadmanindia001@gmail.com>
2019-10-03 16:10:36 +0530

    hello

--- test_app.py
+++ test_app.py
@@ -5,7 +5,7 @@ from app import test
 class TestApp(unittest.TestCase):
 
     def test_test(self):
-        self.assertEqual(test(), "synopsys!\n")
+        self.assertEqual(test(), "cicd!\n")
 
 
 if __name__ == '__main__':
