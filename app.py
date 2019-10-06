e6e2fe1fa35dd665e203b693d60e65ef1b41a50d
DeadManIndia <deadmanindia001@gmail.com>
2019-10-06 11:42:01 +0530

    hello

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-#!/usr/bin/env python
 
-import webapp2
-
-
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
+from flask import Flask
+app = Flask('synopsys')
 
 
-self.response.write('I update automatically!')
+@app.route('/')
+def test():
+    return "synopsys!\n"
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+
+
